17ae8b28072d881e81f772ad6b8c8edf4e15093a
ащч <@SAMSUNG350U2B>
2018-04-21 10:47:14 +0300

    added MVC config

---
+++ src/main/java/com/logos/MvcConfig.java
@@ -0,0 +1,35 @@
+package com.logos;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer{
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addFormatters(registry);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.jsp().prefix("/WEB-INF/views").suffix(".jsp").viewClass(JstlView.class);
+	}
+
+	
+	
+}
