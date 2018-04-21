e488378d12f29ff428499c2d57c26343bb415e1a
ащч <@SAMSUNG350U2B>
2018-04-21 10:37:55 +0300

    first

---
+++ src/main/java/com/logos/ConsultationProjectApplication.java
@@ -0,0 +1,12 @@
+package com.logos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultationProjectApplication.class, args);
+	}
+}
