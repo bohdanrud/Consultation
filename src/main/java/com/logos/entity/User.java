851cbb0723a7941767eada014a5a9f46f336bdb6
ащч <@SAMSUNG350U2B>
2018-04-21 10:55:25 +0300

    added user

---
+++ src/main/java/com/logos/entity/User.java
@@ -0,0 +1,29 @@
+package com.logos.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class User extends BaseEntity{
+
+	@Column(name = "login", nullable = false, unique = true)
+	private String login;
+	
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+}
