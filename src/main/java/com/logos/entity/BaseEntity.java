cb1a3cd8b0b102f4dccc37646633bf717d6af578
ащч <@SAMSUNG350U2B>
2018-04-21 10:51:02 +0300

    added TIMESTAMP

--- src/main/java/com/logos/entity/BaseEntity.java
+++ src/main/java/com/logos/entity/BaseEntity.java
@@ -1,9 +1,16 @@
 package com.logos.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @MappedSuperclass
 public abstract class BaseEntity {
@@ -11,5 +18,10 @@ public abstract class BaseEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@CreationTimestamp
+	@Column(name = "created_at")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createdAt = new Date(); 
 
 }
